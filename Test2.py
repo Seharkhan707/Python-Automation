8f5d7c95656d8122e3d9634228601d4d381bf756
Sehar <Khansehar707@gmail.com>
2025-06-20 12:27:51 +0500

    Add Test2.py - multiplication table program

---
+++ Test2.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter first number for table: "))
+
+print(f"{num1} *1= {num1*1}")
+print(f"{num1} *2= {num1*2}")
+print(f"{num1} *3= {num1*3}")
+print(f"{num1} *4= {num1*4}")
+
+
+
+
