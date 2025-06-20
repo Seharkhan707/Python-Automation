c9af6c1988e80d22f6715bd65d1c8e8649d09eda
Sehar <Khansehar707@gmail.com>
2025-06-20 11:55:27 +0500

    first commit

---
+++ Test1.py
@@ -0,0 +1,19 @@
+##print("Hello Sehar")
+
+a=5
+b=5
+c=a+b
+##print(c)
+
+print("Hello Sehar!\nWelcome to Earn With Sehar.")
+
+print("*****************************************")
+
+user_input1=int(input("Enter First User input : "))
+
+User_input2=int(input("Enter First User input : "))
+
+RESULT=user_input1 +User_input2
+
+print(RESULT)
+
