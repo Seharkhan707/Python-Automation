87dc5ef106547a644801606a5099f17342674dac
Sehar <Khansehar707@gmail.com>
2025-06-21 11:52:36 +0500

    Calculate PI

--- Test2.py
+++ Test2.py
@@ -1,9 +1,23 @@
-num1 = int(input("Enter first number for table: "))
+print("This is String Formating ")
 
-print(f"{num1} *1= {num1*1}")
-print(f"{num1} *2= {num1*2}")
-print(f"{num1} *3= {num1*3}")
-print(f"{num1} *4= {num1*4}")
+
+Table_Input=int(input("Enter Your Table Input Value :"))
+
+print(f"{Table_Input}*1={Table_Input *1}")
+
+print(f"{Table_Input}*2={Table_Input *2}")
+
+print(f"{Table_Input}*3={Table_Input *3}")
+
+print(f"{Table_Input}*4={Table_Input *4}")
+
+print(f"{Table_Input}*5={Table_Input *5}")
+
+
+
+
+
+
 
 
 
---
+++ Test3.py
@@ -0,0 +1,6 @@
+print("Calculate PI Value:")
+r=int(input("Enter Radios value"))
+
+Area= 3.14 *(r*2)
+
+print(Area)
