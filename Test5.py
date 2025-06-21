012e520ab4e26ef6ce3d293bb4483d89bdbba0de
Sehar <Khansehar707@gmail.com>
2025-06-21 12:07:42 +0500

    Check Maximum Num with Condition /max funtion

---
+++ Test5.py
@@ -0,0 +1,15 @@
+print("Check Maximum Num:")
+
+num1=int(input("Enter num 1:"))
+num2=int(input("Enter num 2:"))
+num3=int(input("Enter num 3:"))
+
+
+##if num1>num2:
+    ##print("Num1 is greater")
+
+##else:
+   ## print("Num 2 is greater")
+
+Result=max(num1,num2,num3)
+print(Result)
